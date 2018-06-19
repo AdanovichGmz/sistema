e8f3f35a33301e13b068a23d70af45a4d5aeb81e
AdanovichGmz <adn2290@hotmail.com>
2018-06-19 10:45:28 -0500

    password

--- saves/conexion5s.php
+++ saves/conexion5s.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli=new mysqli("localhost","root","","5s"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
+$mysqli=new mysqli("localhost","root","historias2017","5s"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
 mysqli_set_charset($mysqli,"utf8");
 if(mysqli_connect_errno()){
     echo 'Conexion Fallida : ', mysqli_connect_error();
