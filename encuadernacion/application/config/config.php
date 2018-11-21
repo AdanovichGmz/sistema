53f3489f0ad721fa9d38a86b63c982409a266e46
AdanovichGmz <adn2290@hotmail.com>
2018-11-21 16:30:35 -0600

    encuadernacion

--- encuadernacion/application/config/config.php
+++ encuadernacion/application/config/config.php
@@ -4,14 +4,14 @@ date_default_timezone_set("America/Mexico_City");
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-define('URL', 'http://10.51.28.52/sistema/encuadernacion/');
-define('BASE_URL', 'http://10.51.28.52/sistema/');
+define('URL', 'http://192.168.1.202/sistema/encuadernacion/');
+define('BASE_URL', 'http://192.168.1.202/sistema/');
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'produccion');
+define('DB_NAME', 'master_produccion');
 define('DB_USER', 'root');
 //define('DB_PASS', '');
-define('DB_PASS', 'historias2017');
+define('DB_PASS', '');
 define('TODAY', date("d-m-Y"));
 define('REGION', date_default_timezone_set("America/Mexico_City"));
 
