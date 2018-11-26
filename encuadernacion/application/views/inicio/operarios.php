8fcf22137e0d9b60203d1a9b8589e742e7f17d18
AdanovichGmz <adn2290@hotmail.com>
2018-11-26 14:12:19 -0600

    ..

--- encuadernacion/application/views/inicio/operarios.php
+++ encuadernacion/application/views/inicio/operarios.php
@@ -193,7 +193,7 @@ jQuery214(document).on("click", ".picable", function () {
           type:"POST",
           url:"<?php echo URL; ?>inicio/prepareTasks/",   
           data:jQuery214('#task-form').serialize(), 
-          dataType:"json",
+          //dataType:"json",
           success:function(data){
           	console.log(data); 
           	console.log('regreso'+data.response);
@@ -279,7 +279,7 @@ jQuery214(document).on("click", "#saver", function (){
           type:"POST",
           url:"<?php echo URL; ?>inicio/prepareTasks/",   
           data:jQuery214('#other-form').serialize(), 
-          
+          dataType:"json",
           success:function(data){
             console.log(data); 
             console.log('regreso'+data.response);
