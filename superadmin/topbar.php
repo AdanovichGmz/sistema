d9b81e421630d583398b46c74fcbdfc888caca5f
AdanovichGmz <adn2290@hotmail.com>
2018-11-27 09:38:01 -0600

    5s

--- superadmin/topbar.php
+++ superadmin/topbar.php
@@ -19,7 +19,7 @@
             <!-- <li><a href="eteall.php">General<span class="icon-users"></span></a></li> -->
               <li><a href="modify_ete.php">Reporte ETE por Operario<span class="icon-pencil"></span></a></li>        
           </ul>
-</li><li class="submenu">
+</li><li class="submenu" style="display: none;">
           <a href="#"><span class="icon-clipboard"></span>Reportes 5'S<span class="caret icon-arrow-down6"></span></a>
           <ul class="children">
           <li><a href="listaDiaria.php">Lista diaria de Verificacion<span class="icon-user"></span></a></li>
